f2a1529b2795d886419e2109f970fd8c1aa6291a
lagrandecode <seunogunmolufirst1@gmail.com>
2023-03-03 22:13:03 +0100

    "commit"

--- accident/report/models.py
+++ accident/report/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Report(models.Model):
+    Witness_Name = models.CharField(max_length=100)
+    Witness_Email = models.EmailField()
+    Witness_Phone = models.CharField(max_length=17,blank=True,null=True)
+    
