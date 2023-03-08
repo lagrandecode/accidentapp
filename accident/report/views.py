ac87c5bf2776d22a671502cc7d2c80471354f40a
lagrandecode <seunogunmolufirst1@gmail.com>
2023-03-08 23:20:56 +0100

    "commit"

--- accident/report/views.py
+++ accident/report/views.py
@@ -1,18 +1,21 @@
 from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from rest_framework.parsers import JSONParser
 from .serializer import ReportSerializer
 from .models import Report
 
 # Create your views here.
 
-@csrf_exempt
+@api_view(['GET','POST'])
 def report_list(request):
     if request.method == 'GET':
         report = Report.objects.all()
         serializer = ReportSerializer(report,many=True)
-        return JsonResponse(serializer.data,safe=False)
+        return Response(serializer.data)
     
 
     elif request.method == 'POST':
@@ -20,26 +23,26 @@ def report_list(request):
         serializer = ReportSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data,status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-@csrf_exempt
+@api_view(['GET','PUT','DELETE'])
 def report_detail(request,pk):
     try:
         report = Report.objects.get(id=pk)
     except Report.DoesNotExist:
-        return HttpResponse(status=404)
+        return HttpResponse(status=status.HTTP_400_BAD_REQUEST)
     if request.method =='GET':
         serializer = ReportSerializer(report)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
     elif request.method == 'PUT':
         data = JSONParser().parse(request)
         serializer = ReportSerializer(report,data=data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors,status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
     elif request.method == 'DELETE':
         report.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
